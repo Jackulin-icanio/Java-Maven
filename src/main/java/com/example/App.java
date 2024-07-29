0d13906aeb5793786df84234e4a83dbfee430de4
Jackulin-icanio <jackulin.c@icanio.com>
2024-07-29 13:11:53 +0530

    Hello

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world! Maven");
+        System.out.println("Hello! Maven User");
     }
 }
